2d1a6a4e88a00785c3ccec22f042ade4c0512926
Mark Strother <mcstro1@gmail.com>
2016-07-27 19:49:35 -0400

    Read ifstream into filebuf

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <sstream>
 #include <fftw3.h>
 
 using namespace std;
@@ -10,9 +11,15 @@ int main(){
     // This is the line that needs attention
     ifstream file ("marksComp/config.txt");
 
+    filebuf* sbuf = file.rdbuf();
     string txt;
     if( file.good() ){
-        getline(file,txt);
+        size_t size = sbuf -> pubseekoff(0,file.end,file.in);
+        sbuf -> pubseekpos(0,file.in);
+        char* buffer = new char[size]; 
+        sbuf -> sgetn( buffer, size );
+        txt = string( buffer );
+        delete[] buffer;
     }else{
         txt = "Couldn't find file";
     }
@@ -21,7 +28,12 @@ int main(){
     fftw_complex *in;
 
     cout << txt << endl;
+    cout << "Some more stuff was added to main" << endl;
 
+    // Definitely should have closed the file
+    file.close();
+    
+    
     return 0;
     
 }
